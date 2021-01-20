42ed44fb0f93f2b02f7eb2cdb54d240eb05daff4
Abiola Osinupebi <abiolaosinupebicsa@gmail.com>
2021-01-20 01:28:32 +0000

    redirect URLs

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080; // default port 8080
 // add ejs: new line
 app.set("view engine", "ejs");
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -13,11 +14,18 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-// makinh post request
+// making post request
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+// route: url shortening (part 2)
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
 });
 
 // route for urls_new.ejs
@@ -73,3 +81,7 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+
+ function generateRandomString() {
+
+}
